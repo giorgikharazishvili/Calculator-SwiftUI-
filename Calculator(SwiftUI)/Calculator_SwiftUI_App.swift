8afcc65986d9618741c25ab6e0a21e17fe6c1e31
giorgikharazishvili <giorgi.kharazishvili@hotmail.com>
2022-12-04 20:18:07 +0400

    Initial Commit

---
+++ Calculator(SwiftUI)/Calculator_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUI_App.swift
+//  Calculator(SwiftUI)
+//
+//  Created by GIORGI's MacPro on 04.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
